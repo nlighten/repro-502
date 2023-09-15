e4817170ada05d7a08bbaebcb86feca08f4e10c5
Richard Swart <richard.swart@klm.com>
2023-09-15 11:41:12 +0200

    add user pool

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -5,10 +5,13 @@ resource "azurerm_kubernetes_cluster" "repro_502" {
   dns_prefix          = "repro-502"
 
   default_node_pool {
-    name           = "default"
-    node_count     = 3
-    vm_size        = "Standard_D4s_v5"
-    vnet_subnet_id = azurerm_subnet.aks.id
+    name                         = "default"
+    node_count                   = 3
+    vm_size                      = "Standard_D4s_v5"
+    vnet_subnet_id               = azurerm_subnet.aks.id
+    os_disk_type                 = "Ephemeral"
+    only_critical_addons_enabled = true
+    temporary_name_for_rotation  = "defaulttmp"
   }
 
   identity {
@@ -19,6 +22,14 @@ resource "azurerm_kubernetes_cluster" "repro_502" {
     network_plugin = "kubenet"
   }
 
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "user" {
+  name                        = "user"
+  kubernetes_cluster_id       = azurerm_kubernetes_cluster.repro_502.id
+  vm_size                     = "Standard_D4s_v5"
+  node_count                  = 3
+  os_disk_type                = "Ephemeral"
 }
 
 resource "azurerm_role_assignment" "aks_on_subnet" {
